376be1f64754201252c62ac9575e787fe724b766
rRhesu <victornzioka420@gmail.com>
2025-06-04 20:22:20 +0300

    Add files via upload

---
+++ InventoryItem.vb
@@ -0,0 +1,9 @@
+﻿Public Class InventoryItem
+    Public Property Id As Integer
+    Public Property Name As String
+    Public Property Category As String
+    Public Property Quantity As Integer
+    Public Property Price As Decimal
+    Public Property CreatedAt As DateTime
+    Public Property UpdatedAt As DateTime
+End Class
