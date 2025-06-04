87f4cb78911e7c5520f738cd1e4d5c05bf9cd95a
rRhesu <victornzioka420@gmail.com>
2025-06-04 20:21:31 +0300

    Add files via upload

---
+++ User.vb
@@ -0,0 +1,5 @@
+﻿Public Class User
+    Public Property Username As String
+    Public Property Password As String ' In production, store hashed!
+    Public Property Role As String ' "admin" or "user"
+End Class
